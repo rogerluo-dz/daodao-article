f3107d6e4124ce08b3abe5b766a50e37d4bad4d7
Roger_Luo <190681145@qq.com>
2022-07-29 17:35:13 +0800

    structure changed

---
+++ examples/StateMachine/spring_statemachine_examples/S01_order_status_example/src/main/java/org/daodao/statemachine/spring/SpringStateMachineApp.java
@@ -0,0 +1,21 @@
+package org.daodao.statemachine.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Roger_Luo
+ */
+@SpringBootApplication
+public class SpringStateMachineApp {
+
+  /**
+   * 应用启动类
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStateMachineApp.class, args);
+  }
+
+}
